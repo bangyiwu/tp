46af6c60b1acdfe785a0db24318bd12118a49e09
Bangyi Wu <bangyi98@gmail.com>
2020-10-28 01:38:38 +0800

    Fix test cases checkstyle

--- src/test/java/seedu/address/logic/commands/contacts/DeleteContactByTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/contacts/DeleteContactByTagCommandTest.java
@@ -1,25 +1,21 @@
 package seedu.address.logic.commands.contacts;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.model.Calendar;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
-import seedu.address.model.tag.TagTreeImpl;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.TagTreeImpl;
 
 /**
  * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests for
